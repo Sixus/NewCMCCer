38749eb91ff9864f7f4e3e86f5a24b59d11d9fa4
SixH <782516426@qq.com>
2019-11-30 10:16:01 +0800

    添加Diary活动及跳转

--- app/src/main/java/com/hicmcc/newcmccer/MainActivity.java
+++ app/src/main/java/com/hicmcc/newcmccer/MainActivity.java
@@ -2,7 +2,10 @@ package com.hicmcc.newcmccer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//test
+        Button diaryButton = (Button)findViewById(R.id.DiaryButton);
+        diaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toDiary = new Intent(MainActivity.this,  DiaryActivity.class);
+                startActivity(toDiary);
+            }
+        });
     }
+
 }
